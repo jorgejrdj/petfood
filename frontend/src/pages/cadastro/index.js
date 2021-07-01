ab412189869ce40d9c54b2b35ddbf39d181b188e
Jorge de Souza <jorgejrdj1@gmail.com>
2021-07-01 11:35:01 -0300

    estilizando os inputs

---
+++ frontend/src/pages/cadastro/index.js
@@ -0,0 +1,37 @@
+import Logo from '../../assets/logo-white.png';
+import Illustration from '../../assets/illustration.png';
+
+const Cadastro = () => {
+  return (
+    <div className="container-fluid h-100 bg-primary">
+      <div className="col-12" >
+        <img src={Logo} />
+      </div>
+      <div className="row">
+        <div className="col-6 text-right my-auto">
+          <img src={Illustration} className="img-fluid" />
+        </div>
+        <div className="col-6">
+          <div className="box">
+            <h2>Falta pouquíssimo para o seu pet ser feliz</h2>
+            <input type="text" className="form-control form-control-bg mb-3"
+              placeholder="Nome Completo" />
+            <input type="text" className="form-control form-control-bg mb-3"
+              placeholder="E-mail" />
+            <input type="text" className="form-control form-control-bg mb-3"
+              placeholder="CPF" />
+            <input type="text" className="form-control form-control-bg mb-3"
+              placeholder="Telefone" />
+            <input type="text" className="form-control form-control-bg mb-3"
+              placeholder="Data de nascimento" />
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+
+}
+
+export default Cadastro;
