def823d8892df303dc9f892bd5f963b79398d698
Jorge de Souza <jorgejrdj1@gmail.com>
2021-07-17 21:36:50 -0300

    'first'

--- frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Cadastro from './pages/cadastro';
-
-import './styles/global.css'
-ReactDOM.render(<Cadastro />, document.getElementById('root'));
